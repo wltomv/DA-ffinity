d8eefecf37b07b13039c046c3709e9bb164873e7
Walter Oswaldo <oswaldo@MacBook-Pro-de-Walter.local>
2022-11-11 17:11:14 -0600

    Add function to create a new post in DAffinity  contract

--- contracts/DAffinity.sol
+++ contracts/DAffinity.sol
@@ -14,4 +14,34 @@ contract DAffinity {
         uint256 likes;
         address author;
     }
+
+    event PostCreated(
+        uint256 id,
+        string ipfsHash,
+        string description,
+        uint256 likes,
+        address author
+    );
+
+    function newPost(string memory _ipsHash, string memory _description)
+        public
+    {
+        // Textual content must exist in the publication
+        require(bytes(_description).length > 0);
+        // User address exists
+        require(msg.sender != address(0x0));
+
+        //Increment image id
+        postCount++;
+        // Add Image to contract
+        posts[postCount] = Post(
+            postCount,
+            _ipsHash,
+            _description,
+            0,
+            msg.sender
+        );
+
+        emit PostCreated(postCount, _ipsHash, _description, 0, msg.sender);
+    }
 }
