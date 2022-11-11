b209b0f857c598bfc4b2bcabe4887872966a8d2d
Walter Oswaldo <oswaldo@MacBook-Pro-de-Walter.local>
2022-11-11 15:24:23 -0600

    Add definition of publication storage in DAffinity.sol

--- contracts/DAffinity.sol
+++ contracts/DAffinity.sol
@@ -3,4 +3,15 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract DAffinity {
     string public name = "DA-ffinity";
+
+    uint256 public postCount = 0;
+    mapping(uint256 => Post) public posts;
+
+    struct Post {
+        uint256 id;
+        string message;
+        string ipfsHash;
+        uint256 likes;
+        address author;
+    }
 }
